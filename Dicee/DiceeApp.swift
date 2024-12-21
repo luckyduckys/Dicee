cff11166fa8a9c7c36b31a0b1405f7a73737520d
luckyduckys <mechammitchell@outlook.com>
2024-12-21 10:13:31 -0700

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Mitch Mecham on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
